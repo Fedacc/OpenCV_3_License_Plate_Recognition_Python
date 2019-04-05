5a2c6eb8610558e0ca1c7e59b4dc649def054521
Fedacc <federico_accetta@it.ibm.com>
2019-04-05 16:24:46 +0200

    update for opencv 4.0.0
    removed interactions
    check all images in folder
    updated for opencv version 4.0.0

--- Main.py
+++ Main.py
@@ -8,6 +8,8 @@ import DetectChars
 import DetectPlates
 import PossiblePlate
 
+image_folder = 'LicPlateImages'
+
 # module level variables ##########################################################################
 SCALAR_BLACK = (0.0, 0.0, 0.0)
 SCALAR_WHITE = (255.0, 255.0, 255.0)
@@ -27,53 +29,75 @@ def main():
         return                                                          # and exit program
     # end if
 
-    imgOriginalScene  = cv2.imread("LicPlateImages/1.png")               # open image
+    # FEDE read and insert all images in a list
+    imagelist = []
+    for root,dir,files in os.walk(image_folder):
+            for file in files:
+                if(file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg')):
+                    print(file)
+                    imagelist.append(file)
 
-    if imgOriginalScene is None:                            # if image was not read successfully
-        print("\nerror: image not read from file \n\n")  # print error message to std out
-        os.system("pause")                                  # pause so user can see error message
-        return                                              # and exit program
-    # end if
-
-    listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)           # detect plates
-
-    listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)        # detect chars in plates
-
-    cv2.imshow("imgOriginalScene", imgOriginalScene)            # show scene image
-
-    if len(listOfPossiblePlates) == 0:                          # if no plates were found
-        print("\nno license plates were detected\n")  # inform user no plates were found
-    else:                                                       # else
-                # if we get in here list of possible plates has at leat one plate
-
-                # sort the list of possible plates in DESCENDING order (most number of chars to least number of chars)
-        listOfPossiblePlates.sort(key = lambda possiblePlate: len(possiblePlate.strChars), reverse = True)
-
-                # suppose the plate with the most recognized chars (the first plate in sorted by string length descending order) is the actual plate
-        licPlate = listOfPossiblePlates[0]
-
-        cv2.imshow("imgPlate", licPlate.imgPlate)           # show crop of plate and threshold of plate
-        cv2.imshow("imgThresh", licPlate.imgThresh)
-
-        if len(licPlate.strChars) == 0:                     # if no chars were found in the plate
-            print("\nno characters were detected\n\n")  # show message
-            return                                          # and exit program
+    for image in imagelist:
+        imgOriginalScene  = cv2.imread('./LicPlateImages/' + image)               # open image
+    
+        if imgOriginalScene is None:                            # if image was not read successfully
+            print("\nerror: image not read from file \n\n")  # print error message to std out
+            os.system("pause")                                  # pause so user can see error message
+            return                                              # and exit program
         # end if
+    
+        listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)           # detect plates
+    
+        listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)        # detect chars in plates
+    
+        # FEDE commentato mostrare immagine
+        # cv2.imshow("imgOriginalScene", imgOriginalScene)            # show scene image
+    
+        if len(listOfPossiblePlates) == 0:                          # if no plates were found
+            print("\nno license plates were detected\n")  # inform user no plates were found
+            if not os.path.exists('to-check'):
+                    os.makedirs('to-check')
+            cv2.imwrite('./to-check/' + image, imgOriginalScene)
+        else:                                                       # else
+                    # if we get in here list of possible plates has at leat one plate
+    
+                    # sort the list of possible plates in DESCENDING order (most number of chars to least number of chars)
+            listOfPossiblePlates.sort(key = lambda possiblePlate: len(possiblePlate.strChars), reverse = True)
+    
+                    # suppose the plate with the most recognized chars (the first plate in sorted by string length descending order) is the actual plate
+            licPlate = listOfPossiblePlates[0]
+    
+            # FEDE commentato mostrare immagine
+            # cv2.imshow("imgPlate", licPlate.imgPlate)           # show crop of plate and threshold of plate
+            # cv2.imshow("imgThresh", licPlate.imgThresh)
+    
+            if len(licPlate.strChars) == 0:                     # if no chars were found in the plate
+                print("\nno characters were detected\n\n")  # show message
+                #FEDE rimosso chiusura programma, passo alla foto successiva
+                # return                                          # and exit program
+                if not os.path.exists('to-check'):
+                    os.makedirs('to-check')
+                cv2.imwrite('./to-check/' + image, imgOriginalScene)
+            else:
+                drawRedRectangleAroundPlate(imgOriginalScene, licPlate)             # draw red rectangle around plate
 
-        drawRedRectangleAroundPlate(imgOriginalScene, licPlate)             # draw red rectangle around plate
+                print("\nlicense plate read from image = " + licPlate.strChars + "\n")  # write license plate text to std out
+                print("----------------------------------------")
 
-        print("\nlicense plate read from image = " + licPlate.strChars + "\n")  # write license plate text to std out
-        print("----------------------------------------")
+                # writeLicensePlateCharsOnImage(imgOriginalScene, licPlate)           # write license plate text on the image
 
-        writeLicensePlateCharsOnImage(imgOriginalScene, licPlate)           # write license plate text on the image
+                # FEDE commentato mostrare immagine
+                # cv2.imshow("imgOriginalScene", imgOriginalScene)                # re-show scene image
 
-        cv2.imshow("imgOriginalScene", imgOriginalScene)                # re-show scene image
+                # FEDE save image to directory
+                if not os.path.exists('plate-removed'):
+                    os.makedirs('plate-removed')
+                cv2.imwrite('./plate-removed/' + image, imgOriginalScene)           # write image out to file
 
-        cv2.imwrite("imgOriginalScene.png", imgOriginalScene)           # write image out to file
+            # end if else
 
-    # end if else
-
-    cv2.waitKey(0)					# hold windows open until user presses a key
+            # FEDE commentato interattività
+            # cv2.waitKey(0)					# hold windows open until user presses a key
 
     return
 # end main
@@ -83,10 +107,14 @@ def drawRedRectangleAroundPlate(imgOriginalScene, licPlate):
 
     p2fRectPoints = cv2.boxPoints(licPlate.rrLocationOfPlateInScene)            # get 4 vertices of rotated rect
 
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[0]), tuple(p2fRectPoints[1]), SCALAR_RED, 2)         # draw 4 red lines
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[1]), tuple(p2fRectPoints[2]), SCALAR_RED, 2)
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[2]), tuple(p2fRectPoints[3]), SCALAR_RED, 2)
-    cv2.line(imgOriginalScene, tuple(p2fRectPoints[3]), tuple(p2fRectPoints[0]), SCALAR_RED, 2)
+    cv2.line(imgOriginalScene, tuple(p2fRectPoints[0]), tuple(p2fRectPoints[1]), (121,126,135), 2)         # draw 4 red lines
+    cv2.line(imgOriginalScene, tuple(p2fRectPoints[1]), tuple(p2fRectPoints[2]), (121,126,135), 2)
+    cv2.line(imgOriginalScene, tuple(p2fRectPoints[2]), tuple(p2fRectPoints[3]), (121,126,135), 2)
+    cv2.line(imgOriginalScene, tuple(p2fRectPoints[3]), tuple(p2fRectPoints[0]), (121,126,135), 2)
+    # FEDE aggiunto rettangolo
+    # cv2.rectangle(imgOriginalScene,tuple(p2fRectPoints[1]),tuple(p2fRectPoints[3]),(121,126,135),-1)
+    contours = np.array( [ list(p2fRectPoints[1]), list(p2fRectPoints[0]), list(p2fRectPoints[3]), list(p2fRectPoints[2]) ], 'int32' )
+    cv2.fillPoly(imgOriginalScene, pts=[contours], color=(121,126,135))
 # end function
 
 ###################################################################################################
